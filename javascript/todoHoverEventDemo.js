19b07c919e4f0c7cbeffb459b9fde84708c28415
Kiselev <j8kiselev@gmail.com>
2020-04-25 08:52:31 +0300

    JS events

---
+++ javascript/todoHoverEventDemo.js
@@ -0,0 +1,17 @@
+var lis = document.querySelectorAll("li");
+
+lis.forEach(function(li){
+
+	li.addEventListener("mouseover", function(){
+		this.classList.add("selected");
+	});
+
+	li.addEventListener("mouseout", function(){
+		this.classList.remove("selected");
+	});
+
+	li.addEventListener("click", function(){
+		this.classList.toggle("done");
+	});
+});
+
