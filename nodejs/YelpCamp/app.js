a3aafc0d37e99b291e2ced1ebe021a5b41e8e796
Kiselev <j8kiselev@gmail.com>
2020-05-16 10:36:20 +0300

    YelpCamp. #8 NEW and CREATE routes for Comments

--- nodejs/YelpCamp/app.js
+++ nodejs/YelpCamp/app.js
@@ -3,8 +3,8 @@ app = express(),
 bodyParser = require("body-parser"),
 mongoose = require("mongoose"),
 Campground = require("./models/campground"),
+Comment = require("./models/comment"),
 seedDB = require("./seeds");
-
 
 const port = 3030;
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
@@ -21,7 +21,7 @@ app.get("/campgrounds", function(req, res) {
 		if(err) {
 			console.log(err);
 		} else {
-			res.render("index", {campgrounds: allCampgrounds});		
+			res.render("campgrounds/index", {campgrounds: allCampgrounds});		
 		}
 	});
 });
@@ -42,7 +42,7 @@ app.post("/campgrounds", function(req, res) {
 });
 
 app.get("/campgrounds/new", function(req, res) {
-	res.render("new");
+	res.render("comments/new");
 });
 
 app.get("/campgrounds/:id", function(req, res) {
@@ -52,10 +52,41 @@ app.get("/campgrounds/:id", function(req, res) {
 			console.log(err);
 		} else {
 			console.log(foundCampground);
-			res.render("show", {campground: foundCampground});
+			res.render("campgrounds/show", {campground: foundCampground});
 		}
 	});
 
+});
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+	
+	Campground.findById(req.params.id, function(err, campground) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("comments/new", {campground: campground});
+		}
+	})
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+
+	Campground.findById(req.params.id, function(err, campground) {
+		if(err) {
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			Comment.create(req.body.comment, function(err, comment) {
+				if(err) {
+					console.log(err);
+				} else {
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			});
+		}
+	});
 });
 
 app.listen(port, function () {
