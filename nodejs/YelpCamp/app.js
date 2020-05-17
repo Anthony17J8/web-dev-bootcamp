69dcda55dc6122abf8c4fedd6879bfccd3a69521
Kiselev <j8kiselev@gmail.com>
2020-05-17 14:22:04 +0300

    YelpCamp. #10 Auth. Sign Up feature

--- nodejs/YelpCamp/app.js
+++ nodejs/YelpCamp/app.js
@@ -2,7 +2,10 @@ var express = require("express"),
 app = express(),
 bodyParser = require("body-parser"),
 mongoose = require("mongoose"),
+passport = require("passport"),
+LocalStrategy = require("passport-local"),
 Campground = require("./models/campground"),
+User = require("./models/user"),
 Comment = require("./models/comment"),
 seedDB = require("./seeds");
 
@@ -12,6 +15,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+	secret: "I love Java",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
 	res.render("landing");
@@ -88,6 +104,30 @@ app.post("/campgrounds/:id/comments", function(req, res) {
 			});
 		}
 	});
+});
+
+// AUTH ROUTES
+
+// show register form
+app.get("/register", function(req, res) {
+	res.render("register");
+})
+
+
+// handle sign up logic
+app.post("/register", function(req, res) {
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user) {
+		if(err) {
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req, res, function() {
+			res.redirect("/campgrounds");
+		})
+	});
+
+
 });
 
 app.listen(port, function () {
