9f9c53a5654e4ae20b421aef734994f08509354f
Kiselev <j8kiselev@gmail.com>
2020-05-19 19:58:26 +0300

    Refactoring code

---
+++ nodejs/YelpCamp/routes/campgrounds.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var router = express.Router();
+var Campground = require("../models/campground");
+
+// INDEX - show all campgrounds
+router.get("/", function(req, res) {
+
+	Campground.find({}, function(err, allCampgrounds) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds/index", {campgrounds: allCampgrounds});		
+		}
+	});
+});
+
+// CREATE - add new campground to db
+router.post("/", function(req, res) {
+	var name = req.body.name;
+	var image = req.body.image;
+	var description = req.body.description;
+	var newCampgound = {name: name, image: image, description: description};
+	Campground.create(newCampgound, function(err, newlyCreated) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+// NEW - show form to create camground
+router.get("/new", function(req, res) {
+	res.render("comments/new");
+});
+
+// SHOW - shows more info about one campground
+router.get("/:id", function(req, res) {
+
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log(foundCampground);
+			res.render("campgrounds/show", {campground: foundCampground});
+		}
+	});
+});
+
+module.exports = router;
