0637a16bfef9531e46187a2d924c3abbd58787d3
Kiselev <j8kiselev@gmail.com>
2020-05-21 20:08:20 +0300

    YelpCamp. #16 Edit campground feature

--- nodejs/YelpCamp/app.js
+++ nodejs/YelpCamp/app.js
@@ -4,6 +4,7 @@ bodyParser = require("body-parser"),
 mongoose = require("mongoose"),
 passport = require("passport"),
 LocalStrategy = require("passport-local"),
+methodOverride = require("method-override"),
 Campground = require("./models/campground"),
 User = require("./models/user"),
 Comment = require("./models/comment"),
@@ -19,6 +20,7 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 // seedDB();
 
 // PASSPORT CONFIGURATION
