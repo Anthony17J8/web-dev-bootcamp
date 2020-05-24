52c381612a11408350db2077d1842771b64ca735
Kiselev <j8kiselev@gmail.com>
2020-05-24 09:29:09 +0300

    YelpCamp. #20 Added flash messages

--- nodejs/YelpCamp/app.js
+++ nodejs/YelpCamp/app.js
@@ -1,33 +1,35 @@
 var express = require("express"),
-app = express(),
-bodyParser = require("body-parser"),
-mongoose = require("mongoose"),
-passport = require("passport"),
-LocalStrategy = require("passport-local"),
-methodOverride = require("method-override"),
-Campground = require("./models/campground"),
-User = require("./models/user"),
-Comment = require("./models/comment"),
-seedDB = require("./seeds");
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    flash = require("connect-flash"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground = require("./models/campground"),
+    User = require("./models/user"),
+    Comment = require("./models/comment"),
+    seedDB = require("./seeds");
 
 // requiring routes
 var commentRoutes = require("./routes/comments"),
-campgroundRoutes = require("./routes/campgrounds"),
-indexRoutes = require("./routes/index");
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes = require("./routes/index");
 
 const port = 3030;
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 // seedDB();
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
-	secret: "I love Java",
-	resave: false,
-	saveUninitialized: false
+    secret: "I love Java",
+    resave: false,
+    saveUninitialized: false
 }));
 
 app.use(passport.initialize());
@@ -37,9 +39,11 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // will for every single route
-app.use(function(req, res, next) {
-	res.locals.currentUser = req.user;
-	next();
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
 });
 
 app.use(indexRoutes);
@@ -47,5 +51,5 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
 app.listen(port, function () {
-	console.log("The server has started");
+    console.log("The server has started");
 });
